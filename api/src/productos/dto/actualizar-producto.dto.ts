2a47112c8f9bb78ff6736a7c4af5e52bd6e3bc57
jonsanso <91910562+jonathansansok@users.noreply.github.com>
2026-07-22 23:55:17 -0300

    feat- docs de endpoints y transform en  validacion de query

--- api/src/productos/dto/actualizar-producto.dto.ts
+++ api/src/productos/dto/actualizar-producto.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CrearProductoDto } from './crear-producto.dto';
 
+// PartialType de swagger: hereda validaciones y ademas documenta los campos en /docs
 export class ActualizarProductoDto extends PartialType(CrearProductoDto) {}
